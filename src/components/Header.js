09b1b310c652a5012b9ba1dc5c4228ea4216f73a
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-02-03 14:53:43 +0530

    Some bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../utils/chatflix-logo1.png';
-import { signOut } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../utils/Firebase';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
   const handleSignOut = () =>{
     signOut(auth).then(() => {
-      navigate('/')
     })
     .catch((error) => {
       navigate('/error')
     })
-  }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,(user) => {
+        if(user){
+            const {uid, email, displayName} = user;
+            dispatch(
+              addUser({uid:uid, email:email, displayName:displayName})
+            )
+            navigate("/browse");
+        }
+        else{
+            dispatch(removeUser());
+            navigate("/");
+        }
+    });
+
+    //unsubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="absolute w-screen  bg-red-800 p-2 flex justify-between items-center">
       <img className="w-36 h-auto object-contain" src={logo} alt="Chatflix Logo" />
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,6 @@ import Header from './Header';
 import {checkValidData} from '../utils/Validate'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../utils/Firebase';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 
@@ -11,7 +10,6 @@ const Login = () => {
 
     const [isSignInForm,setIsSignInForm] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null);
-    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const name = useRef(null);
@@ -39,7 +37,6 @@ const Login = () => {
                 .then(() => {
                     const {uid, email, displayName} = auth.currentUser;
                     dispatch(addUser({uid:uid, email:email, displayName:displayName}))
-                    navigate('/browse')
                 })
                 .catch((error) => {
                     setErrorMessage(error.message)
@@ -59,8 +56,7 @@ const Login = () => {
             signInWithEmailAndPassword(auth, email.current?.value,password.current?.value)
             .then((userCredential) => {
                 const user = userCredential.user
-                console.log(user)
-                navigate('/browse')
+                
             })
             .catch((error) => {
                 const errorCode = error.code;
