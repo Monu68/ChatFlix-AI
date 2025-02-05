de6bb1324b881720901ec147d4a5725ba7668fd7
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-02-05 23:05:00 +0530

    TMDB api check

---
+++ src/utils/constant.js
@@ -0,0 +1,13 @@
+
+export const USER_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+
+export const API_OPTION = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTRmZWMzNTE1M2FlM2ZhMjA4ODk1Yzk4ZjIxMGI4NiIsIm5iZiI6MTczODc3MDQxNy4xMzkwMDAyLCJzdWIiOiI2N2EzODdmMWUxN2RkNGZiZDg4MTFkMTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.sv9enJjrt3zBL9YJ6S21rJoFeJg-WTELZrd5LCtvV9o'
+    }
+  };
+
+
+  
