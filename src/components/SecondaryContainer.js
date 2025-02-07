c019d3b05d8d5f5cd06f8c9740bb12528ec1c24e
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-02-07 23:27:32 +0530

    Movie card

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieList from "./MovieList"
 
 const SecondaryContainer = () => {
+
+    const movies = useSelector(store => store.movies)
   return (
-    <div>SecondaryContainer</div>
+    <div>
+        <MovieList title ={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title ={"Trending"} movies={movies.nowPlayingMovies} />
+        <MovieList title ={"Popular"} movies={movies.nowPlayingMovies} />
+        <MovieList title ={"Upcoming"} movies={movies.nowPlayingMovies} />
+        <MovieList title ={"Horror"} movies={movies.nowPlayingMovies} />
+
+    </div>
   )
 }
 
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -9,5 +9,7 @@ export const API_OPTION = {
     }
   };
 
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
+
 
   
